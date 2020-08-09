4f6133cc64e47cb5608395f356bef23ec778f5fd
uwaisalqadri <uwaisalqadri654321@gmail.com>
2020-08-09 17:02:47 +0700

    fav item

---
+++ app/src/main/java/com/masuwes/githubuserfinal/helper/FavouriteItem.kt
@@ -0,0 +1,24 @@
+package com.masuwes.githubuserfinal.helper
+
+import android.database.Cursor
+import com.masuwes.githubuserfinal.db.DatabaseContract.GitColumns.Companion.AVATAR
+import com.masuwes.githubuserfinal.db.DatabaseContract.GitColumns.Companion.LOGIN_NAME
+import com.masuwes.githubuserfinal.db.DatabaseContract.GitColumns.Companion._ID
+import com.masuwes.githubuserfinal.db.DatabaseContract.getColumnInt
+import com.masuwes.githubuserfinal.db.DatabaseContract.getColumnString
+
+class FavouriteItem (cursor: Cursor?) {
+    var id : Int? = 0
+    var username: String? = null
+    var avatar: String? = null
+
+    init {
+        id = getColumnInt(cursor, _ID)
+        username = getColumnString(cursor, LOGIN_NAME)
+        avatar = getColumnString(cursor, AVATAR)
+    }
+
+    override fun toString(): String {
+        return """FavouritesItem{id = '$id',username = '$username',avatar = '$avatar'}"""
+    }
+}
